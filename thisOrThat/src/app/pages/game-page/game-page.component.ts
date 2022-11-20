23e42387aac39a8e2a5ab937aa9854d9c1730a9d
Ael <bluebayband@gmail.com>
2022-11-20 16:37:27 -0500

    configure firebase

--- thisOrThat/src/app/pages/game-page/game-page.component.ts
+++ thisOrThat/src/app/pages/game-page/game-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
 
 @Component({
   selector: 'app-game-page',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataSvc: DataService) { }
 
   ngOnInit(): void {
   }
