4d6cf7e7014cfc957e332384f0b7faf2807df28f
Ael <bluebayband@gmail.com>
2022-12-01 14:45:45 -0500

    implement round 1

--- thisOrThat/src/app/pages/game-page/game-page.component.ts
+++ thisOrThat/src/app/pages/game-page/game-page.component.ts
@@ -2,27 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { ImageRec } from 'src/app/services/data.service';
 
-
 @Component({
   selector: 'app-game-page',
   templateUrl: './game-page.component.html',
-  styleUrls: ['./game-page.component.scss']
+  styleUrls: ['./game-page.component.scss'],
 })
 export class GamePageComponent implements OnInit {
   imageArr: ImageRec[] = [];
-  sampleImg: ImageRec[] = [];
+  currentPair: ImageRec[] = [];
+  chosenImage: ImageRec[] = [];
+
+  roundFinished: boolean = false;
+
+  startIdx = 0;
+  endIdx = 2;
 
   constructor(private dataSvc: DataService) {
     this.dataSvc.imageRecords$.subscribe((res) => {
       this.imageArr = res;
-      this.sampleImg = this.imageArr.slice(1, 3);
-    })
+      this.currentPair = this.imageArr.slice(0, 2);
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onClick = () => {
-    console.log("image clicked!");
-  }
+  onChooseImage = (i: number): void => {
+    // Play round
+    if (this.startIdx < this.imageArr.length) {
+      console.log(this.startIdx, this.endIdx);
+      // add to chosen images
+      this.chosenImage.push(this.currentPair[i]);
+      // update indices
+      this.startIdx = this.endIdx;
+      this.endIdx += 2;
+      // render next pair
+      this.currentPair = this.imageArr.slice(this.startIdx, this.endIdx);
+
+      if (this.chosenImage.length * 2 === this.imageArr.length) {
+        console.log('reset mode!!');
+        this.imageArr = [...this.chosenImage];
+        console.log(this.imageArr);
+        this.chosenImage = this.imageArr.slice(0, 2);
+        this.currentPair = [];
+        this.startIdx = 0;
+        this.endIdx = 2;
+      }
+    }
+  };
 }
